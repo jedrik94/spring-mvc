fdf67a13bbad708f4816f18e1934719f12e16802
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2018-12-03 15:51:57 +0100

    Used @RequestParm binding.

--- src/main/java/pl/jedrik94/demo/controller/HelloController.java
+++ src/main/java/pl/jedrik94/demo/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,12 +22,8 @@ public class HelloController {
     }
 
     @RequestMapping(value = "/processFormUpper")
-    public String showFormToUpperCase(HttpServletRequest request, Model model) {
-        String name = request.getParameter("studentName");
-
-        name = name.toUpperCase();
-
-        String result = "U got that, " + name + "!";
+    public String showFormToUpperCase(@RequestParam(value = "studentName") String name, Model model) {
+        String result = "U got that, " + name.toUpperCase() + "!";
 
         model.addAttribute("message", result);
 
