b4717a86856fae656163e1dab73a8991a249de16
Wojtkowiak Jedrzej <jedrik94@gmail.com>
2018-12-04 09:39:49 +0100

    Implemented parent @RequestMapping in controller.

--- src/main/java/pl/jedrik94/demo/controller/HelloController.java
+++ src/main/java/pl/jedrik94/demo/controller/HelloController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
+@RequestMapping("/hey")
 public class HelloController {
 
     @RequestMapping(value = "/showForm", method = RequestMethod.GET)
